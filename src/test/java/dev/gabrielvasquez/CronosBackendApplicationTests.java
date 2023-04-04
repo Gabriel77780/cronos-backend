402bf5fa00d4f1211e9a1022f5533c159bae2ac8
gabrielvasquez <gabriel77780@hotmail.com>
2023-04-03 23:22:53 -0600

    Initial commit :tada:

---
+++ src/test/java/dev/gabrielvasquez/CronosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielvasquez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
